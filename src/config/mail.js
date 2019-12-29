bf163252f8bab74c35bc6e8eb5885038bda2f5bd
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-28 23:08:58 -0300

    Configuração nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0e0f124decad2d',
+    pass: 'fde54913d77306',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
