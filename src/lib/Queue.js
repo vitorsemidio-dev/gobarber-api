aefb3b6b8cb7a724f83f5b2e208fc86087c9f68c
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-29 09:12:01 -0300

    RedisConfig e Metodos da fila

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -1,11 +1,47 @@
 import Bee from 'bee-queue';
 
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
 class Queue {
   constructor() {
     this.queues = {};
   }
 
-  init() {}
+  /**
+   * Instância que conecta com o Redis
+   * Método que processa as filas
+   */
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  /**
+   * Adiciona novos jobs à fila
+   */
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  /**
+   * Processa cada job que está na fila em background
+   */
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
 }
 
 export default new Queue();
