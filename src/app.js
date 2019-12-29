a0c64358a62f32ca8560b4d4d32799e7271194e2
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-29 14:51:04 -0300

    Substituição variáveis ambiente

--- src/app.js
+++ src/app.js
@@ -38,9 +38,13 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      const erros = await new Youch(err, req).toJSON();
+      if (process.env.NODE_ENV === 'development') {
+        const erros = await new Youch(err, req).toJSON();
 
-      return res.status(500).json(erros);
+        return res.status(500).json(erros);
+      }
+
+      return res.status(500).json({ error: 'Internal Server Error' });
     });
   }
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://9ae372d25916418c91d15643b2a2fe12@sentry.io/1868279',
+  dsn: process.env.SENTRY_DSN,
 };
